b6904c062d1bd1a5d5ad9db1e7fc60b4bf9ebfd1
Laure Bluzet <laure.bluzet@gmail.com>
2019-11-22 17:13:55 +0100

    quete 4

---
+++ src/Controller/WildController.php
@@ -0,0 +1,26 @@
+<?php
+// src/Controller/WildController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WildController extends AbstractController
+{
+    /*public function index() :Response
+    {
+        return new Response(
+            '<html><body>Wild Series Index</body></html>'
+        );
+    }*/
+    /**
+     * @Route("/wild", name="wild_index")
+     */
+    public function index() :Response
+    {
+        return $this->render('wild/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+}
